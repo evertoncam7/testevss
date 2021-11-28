2061c55f29af8f5a8da22c16b559e48c2580488a
evertoncam7 <everton.camargo.cam7@gmail.com>
2021-11-28 11:54:39 -0300

    deploy app-vss

---
+++ banco.js
@@ -0,0 +1,87 @@
+
+
+const data = [
+    {id:0, name:'Homem de família - Gustavo Lima', instruments:[
+
+    ], partes:[
+        {id:00, title:'Introdução', inicial:0, final:1},
+        {id:00, title:'Te olhei você me olhou...', inicial:0, final:1},
+        {id:00, title:'Até você aparecer do nada...', inicial:0, final:1},
+    ]},
+    {id:1, name:'Propaganda - Jorge e Mateus', instruments:[
+
+    ], partes:[
+        {id:00, title:'Introdução...', inicial:0, final:1},
+        {id:00, title:'Ela queima o arroz...', inicial:0, final:1},
+        {id:00, title:'É isso que eu falo pros outros...', inicial:0, final:1},
+        {id:00, title:'Tá doido que eu vou fazer prropaganda de você...', inicial:0, final:1},
+    ]},
+    {id:2, name:'Arranhão - Henrique e Juliano', dir:"arranhao_henrique_e_juliano", cTime:[
+        2,
+        5,
+        18,
+        31,
+        45,
+        58,
+        70,
+        81,
+        95,
+        108,
+        113
+    ] ,instruments:[
+
+        "PIANO-01",
+        "PIANO-02",
+        "VIOLAO-L-MIC",
+        "VIOLAO-L-LINE",
+        "VIOLAO-R-MIC",
+        "VIOLAO-R-LINE",
+        "TONS",
+        "SANFONA",
+        "SANFONA-02",
+        "SURDO",
+        "SHAKER",
+        "STRING",
+        "PERCUSSAO",
+        "CONGAS",
+        
+    ], partes:[
+        {id:00, title:'Introdução...', inicial:0, final:1},
+        {id:00, title:'Parece que se sente cheiro de bebida de ontem sabe que eu tô fraco aí que entra seu nome na tela do celular, me perguntando onde você esta?', inicial:0, final:1},
+        {id:00, title:'Sabe onde minha saudade aperta Quando seu dedo aperta na palavra que me acerta E deixa minha raiva cega Cê sabe que eu sou incapaz e a falta que faz', inicial:0, final:1},
+        {id:00, title:'Aí que mora o perigo Aí que eu caio lindo Aí que eu sei das consequências, mesmo assim vou indo É que vale à pena, vale a cama, vale o risco O que é um arranhão pra quem já tá fudido?', inicial:0, final:1},
+        {id:00, title:'Aí que mora o perigo Aí que eu caio lindo Aí que eu sei das consequências, mesmo assim vou indo É que vale à pena, vale a cama, vale o risco O que é um arranhão pra quem já tá fudido?', inicial:0, final:1},
+        {id:00, title:'Oh, oh', inicial:0, final:1},
+        {id:00, title:'Sabe onde minha saudade aperta Quando seu dedo aperta na palavra que me acerta E deixa minha raiva cega Cê sabe que eu sou incapaz e a falta que faz', inicial:0, final:1},
+        {id:00, title:'Aí que mora o perigo Aí que eu caio lindo Aí que eu sei das consequências, mesmo assim vou indo É que vale à pena, vale a cama, vale o risco O que é um arranhão pra quem já tá fudido?', inicial:0, final:1},
+        {id:00, title:'Aí que mora o perigo Aí que eu caio lindo Aí que eu sei das consequências, mesmo assim vou indo É que vale à pena, vale a cama, vale o risco O que é um arranhão pra quem já tá fudido?', inicial:0, final:1},
+        {id:00, title:'Oh, oh', inicial:0, final:1},
+        {id:00, title:'É que vale à pena, vale a cama, vale o risco O que é um arranhão pra quem já tá fudido?', inicial:0, final:1},
+    
+    ]},
+    {id:3, name:'Você beberia ou não beberia - Zé Neto e Cristiano', dir:"voce_beberia_ou_nao_beberia_ze_neto_e_cristiano", cTime:[
+        2,
+        15,
+        28,
+        41,
+        47
+    ], instruments:[
+
+        "PIANO",
+        "SANFONA-01",
+        "SANFONA-02",
+        "PERCUSSAO",
+        "VIOLAO-L-MIC",
+        "VIOLAO-L-LINE",
+        "VIOLAO-R-MIC",
+        "VIOLAO-R-LINE",
+
+    ], partes:[
+        {id:00, title:'Introdução', inicial:0, final:1},
+        {id:00, title:'A culpa é da saudade Que causa esse sufoco Tá saindo pelos poros E me desmanchando aos poucos', inicial:0, final:1},
+        {id:00, title:'Dói ver ela me esquecendo Então não venha criticar A vida que eu tô vivendo', inicial:0, final:1},
+        {id:00, title:'Me diz aí Se você visse alguém beijando o amor da sua vida Você beberia ou não beberia? Você beberia ou não beberia?', inicial:0, final:1},
+        
+    ]},
+
+];
